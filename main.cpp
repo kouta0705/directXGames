33d3c093fb3574a665ad44bdafdc2c5a5433470f
LC1D_17_takahashi_kouta <kouta180705@gmail.com>
2026-04-14 16:12:02 +0900

    プロジェクトファイルを追加。

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<Windows.h>
+
+//windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	//出力ウィンドウへの文字出力
+	OutputDebugStringA("Hello, DirectX!\n");
+	return 0;
+}
