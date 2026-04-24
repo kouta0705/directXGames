41fd50a5502fc129861bb55be7df833b26c57e7f
LC1D_17_takahashi_kouta <kouta180705@gmail.com>
2026-04-24 16:47:47 +0900

    g

--- main.cpp
+++ main.cpp
@@ -1,9 +1,113 @@
 #include<Windows.h>
+#include<cstdint>
+#include <string>
 
-//windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
+
+
+LRESULT CALLBACK WndowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	//windowsアプリでのエントリーポイント(main関数)
+
+
+
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	}
+
+
+
+
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+
+
+	WNDCLASS wc{};
+
+	wc.lpfnWndProc = WndowProc;
+	wc.lpszClassName = L"CG2WindowClass";
+	wc.hInstance = GetModuleHandle(nullptr);
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	RegisterClass(&wc);
+
+	const int32_t KClientWidth = 1280;
+	const int32_t KClientHeight = 720;
+
+	RECT wrc = { 0,0,KClientWidth,KClientHeight};
+
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hwnd = CreateWindow(
+
+		wc.lpszClassName,
+		L"CG2",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr);
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+
+		}
+	}
+
+
+
 	//出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello, DirectX!\n");
-	int spase = 0;
 	return 0;
+
 }
