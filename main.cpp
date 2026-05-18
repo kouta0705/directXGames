9878513ec3ca2bb26bc7083b4264afb6d7f9ff41
高橋　幸汰 <K025G0099@g.neec.ac.jp>
2026-05-18 21:44:07 +0900

    a

--- main.cpp
+++ main.cpp
@@ -40,34 +40,17 @@ std::string ConvertString(const std::wstring& str) {
 }
 
 
-
-
 LRESULT CALLBACK WndowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	//windowsアプリでのエントリーポイント(main関数)
-
-	
-
 	switch (msg)
 	{
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
-
 	}
-
-
-
-
 	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
-
-
-
-std::string str0{ "STRING!!!" };
-
-std::string str1{ std::to_string(10) };
 
 void Log(const std::string& Message)
 {
@@ -77,24 +60,34 @@ void Log(const std::string& Message)
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
 
+	OutputDebugStringA("Hello, DirectX!\n");
 
-	WNDCLASS wc{};
+	std::string str0{ "STRING!!!" };
+	std::string str1{ std::to_string(10) };
+	Log(str0 + "\n");
+	Log(str1 + "\n");
 
+	int enemyHp = 100;        // ← 初期化
+	int texturePath = 1;      // ← 初期化
+	Log(std::format("enemyHp:{},texturePath:{}\n", enemyHp, texturePath));
+
+	int wstringValue = 123;
+	Log(ConvertString(std::format(L"wstringValue: {}\n", wstringValue)));
+
+	WNDCLASSW wc{};            // ← WNDCLASSW に変更
 	wc.lpfnWndProc = WndowProc;
 	wc.lpszClassName = L"CG2WindowClass";
 	wc.hInstance = GetModuleHandle(nullptr);
 	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	RegisterClass(&wc);
+	RegisterClassW(&wc);       // ← RegisterClassW に変更
 
 	const int32_t KClientWidth = 1280;
 	const int32_t KClientHeight = 720;
 
-	RECT wrc = { 0,0,KClientWidth,KClientHeight};
-
+	RECT wrc = { 0,0,KClientWidth,KClientHeight };
 	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
-	HWND hwnd = CreateWindow(
-
+	HWND hwnd = CreateWindowW(  // ← CreateWindowW に変更
 		wc.lpszClassName,
 		L"CG2",
 		WS_OVERLAPPEDWINDOW,
@@ -107,49 +100,38 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		wc.hInstance,
 		nullptr);
 
+	if (hwnd == nullptr) return 0;  // ← null チェック追加
+
 	ShowWindow(hwnd, SW_SHOW);
 
 	MSG msg{};
-	
-	int enemyHp;
-	int texturePath;
-
-	Log(std::format("enemyHp:{},texturePath:{}\n", enemyHp, texturePath));
 
 	IDXGIFactory7* dxgiFactory = nullptr;
-
-	HRESULT hr = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
-
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));  // ← Factory1 → Factory
 	assert(SUCCEEDED(hr));
 
 	IDXGIAdapter4* useAdapter = nullptr;
-
-	for (UINT i = 0;dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; ++i)
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; ++i)
 	{
 		DXGI_ADAPTER_DESC3 adapterDesc{};
-
 		hr = useAdapter->GetDesc3(&adapterDesc);
 		assert(SUCCEEDED(hr));
 
-		if (!(adapterDesc.Flags&DXGI_ADAPTER_FLAG3_SOFTWARE))
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE))
 		{
 			Log(ConvertString(std::format(L"Use Adapter: {}\n", adapterDesc.Description)));
 			break;
 		}
-
 		useAdapter = nullptr;
-		
 	}
-	
 	assert(useAdapter != nullptr);
 
 	ID3D12Device* device = nullptr;
 	D3D_FEATURE_LEVEL featureLevels[] =
 	{ D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0 };
-
 	const char* fetureLevelString[] = { "12.2","12.1","12.0" };
 
-	for (size_t i = 0;i < std::size(featureLevels);++i)
+	for (size_t i = 0; i < std::size(featureLevels); ++i)
 	{
 		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
 		if (SUCCEEDED(hr))
@@ -158,26 +140,24 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 			break;
 		}
 	}
+	assert(device != nullptr);
+	Log("Complete create D3D12Device!!!\n");
 
-	Log("conmplete Create D3D12Dvice\n");
 	ID3D12CommandQueue* commandQueue = nullptr;
 	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
 	hr = device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue));
-	IID_PPV_ARGS(&commandQueue);
-	assert(hr);
+	// ← 重複していた IID_PPV_ARGS 行を削除
+	assert(SUCCEEDED(hr));
 
 	ID3D12CommandAllocator* commandAllocator = nullptr;
 	hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator));
-
 	assert(SUCCEEDED(hr));
 
 	ID3D12GraphicsCommandList* commandList = nullptr;
-
 	hr = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator, nullptr, IID_PPV_ARGS(&commandList));
-
 	assert(SUCCEEDED(hr));
 
-	IDXGISwapChain* swapChain = nullptr;
+	IDXGISwapChain4* swapChain = nullptr;
 	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
 	swapChainDesc.Width = KClientWidth;
 	swapChainDesc.Height = KClientHeight;
@@ -186,7 +166,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	swapChainDesc.BufferCount = 2;
 	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-	
 	hr = dxgiFactory->CreateSwapChainForHwnd(commandQueue, hwnd, &swapChainDesc, nullptr, nullptr, reinterpret_cast<IDXGISwapChain1**>(&swapChain));
 	assert(SUCCEEDED(hr));
 
@@ -197,7 +176,21 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	hr = device->CreateDescriptorHeap(&rtvDescriptorHeapDesc, IID_PPV_ARGS(&rtvDescriptorHeap));
 	assert(SUCCEEDED(hr));
 
+	ID3D12Resource* swapChainResources[2] = { nullptr };
+	hr = swapChain->GetBuffer(0, IID_PPV_ARGS(&swapChainResources[0]));
+	assert(SUCCEEDED(hr));
+	hr = swapChain->GetBuffer(1, IID_PPV_ARGS(&swapChainResources[1]));  // ← 追加
+	assert(SUCCEEDED(hr));
 
+	D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
+	rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+	D3D12_CPU_DESCRIPTOR_HANDLE rtvStartHandle = rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2];
+	rtvHandles[0] = rtvStartHandle;
+	device->CreateRenderTargetView(swapChainResources[0], &rtvDesc, rtvHandles[0]);
+	rtvHandles[1].ptr = rtvHandles[0].ptr + device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	device->CreateRenderTargetView(swapChainResources[1], &rtvDesc, rtvHandles[1]);
 
 	while (msg.message != WM_QUIT)
 	{
@@ -208,14 +201,25 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		}
 		else
 		{
+			UINT backBufferIndex = swapChain->GetCurrentBackBufferIndex();
 
+			// ← ResourceBarrier を削除（新コードにないため）
+
+			commandList->OMSetRenderTargets(1, &rtvHandles[backBufferIndex], false, nullptr);
+			float clearColor[] = { 0.1f, 0.25f, 0.5f, 1.0f };
+			commandList->ClearRenderTargetView(rtvHandles[backBufferIndex], clearColor, 0, nullptr);
+			hr = commandList->Close();
+			assert(SUCCEEDED(hr));
+
+			ID3D12CommandList* commandLists[] = { commandList };
+			commandQueue->ExecuteCommandLists(1, commandLists);
+			swapChain->Present(1, 0);
+			hr = commandAllocator->Reset();
+			assert(SUCCEEDED(hr));
+			hr = commandList->Reset(commandAllocator, nullptr);
+			assert(SUCCEEDED(hr));
 		}
 	}
 
-
-
-	//出力ウィンドウへの文字出力
-	OutputDebugStringA("Hello, DirectX!\n");
 	return 0;
-
 }
